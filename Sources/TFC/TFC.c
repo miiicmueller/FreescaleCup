7d76a0fc4178961d73faabbe80a4c08e1dca2ffe
CyrilleSavy <cyrille.savy@he-arc.ch>
2013-10-24 09:52:01 +0200

    ajout de l'ajustement du temps d'exposition du capteur CCD par potentiometre dans le mode 3

--- Sources/TFC/TFC.c
+++ Sources/TFC/TFC.c
@@ -1,27 +1,26 @@
 #include "TFC\TFC.h"
 
-
 void TFC_Init()
-{
-	TFC_InitClock();
-	TFC_InitSysTick();
-	TFC_InitGPIO();
-	TFC_InitServos();
-	TFC_InitMotorPWM();
+    {
+    TFC_InitClock();
+    TFC_InitSysTick();
+    TFC_InitGPIO();
+    TFC_InitServos();
+    TFC_InitMotorPWM();
     TFC_InitADCs();
     TFC_InitLineScanCamera();
     TFC_InitTerminal();
-	TFC_InitUARTs();
-	TFC_HBRIDGE_DISABLE;
-	TFC_SetMotorPWM(0,0);
-	
-}
+    TFC_InitUARTs();
+    TFC_HBRIDGE_DISABLE;
+    TFC_SetMotorPWM(0, 0);
+
+    }
 
 void TFC_Task()
-{
-	#if defined(TERMINAL_USE_SDA_SERIAL)
-		TFC_UART_Process();
-	#endif
-	 
+    {
+#if defined(TERMINAL_USE_SDA_SERIAL)
+    TFC_UART_Process();
+#endif
+
     TFC_ProcessTerminal();
-}
+    }
