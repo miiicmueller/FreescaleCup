2cb3bcd92b2491e627295747eeecdbec31f6cdb2
CyrilleSavy <cyrille.savy@he-arc.ch>
2013-10-31 15:59:36 +0100

    debuggage de tTools.c et mTrackline.c codage de tPID.c

--- Project_Headers/Modules/mTrackLine.h
+++ Project_Headers/Modules/mTrackLine.h
@@ -10,19 +10,12 @@
 //fichiers utilises
 #include "TFC\TFC.h"
 
-//definitions de types et structures
-typedef struct
-    {
-	uint16_t location;
-	uint16_t length;
-    } mTrackLineObject;
-
 //--------------------------------------------------------
 // trouve la position de la ligne sur l'image de la camera
 // parametre de retour	: bool : true si la ligne est presente, false sinon
 // parametres : tab	: adresse du tableau a traiter
 //		size	: longueur du tableau
 //--------------------------------------------------------
-bool mFindLine(int16_t* tab, uint16_t size);
+bool mTrackLine_FindLine(int16_t* tab, uint16_t size, uint16_t* thePosition);
 
 #endif /* TTRACKLINE_H_ */
---
+++ Sources/Tools/tPID.c
@@ -0,0 +1,36 @@
+//*****************************************************************************
+//Nom du fichier : tPID.c
+//Auteur et Date : SAVY Cyrille 31.10.2013
+//But : fonction implementant un regulateur PID, avec la structure permettant
+//	de stocker les donnees du regulateur
+//*****************************************************************************
+
+//fichiers utilises
+#include "Tools/tPID.h"
+
+//--------------------------------------------------------
+// regulateur PID
+// parametre de retour	: int16_t : la valeur de la commande a assigner
+// parametres : thePIDStruct	  : structure contenant toutes les 
+//				    informations necessaires au regulateur (cf. tPID.h)
+//--------------------------------------------------------
+int16_t tPID(tPIDStruct* thePIDStruct, int16_t theMesure)
+    {
+    //calcul de la nouvelle erreur
+    //la mesure doit etre convertie dans la grandeur de la consigne
+    int16_t nouvelleErreur = thePIDStruct->consigne
+	    - (thePIDStruct->conversionMesureConsigne * theMesure);
+
+    //integration des erreurs successives
+    thePIDStruct->sommeErreurs += nouvelleErreur;
+
+    //nouvelle consigne avec un regulateur PID
+    thePIDStruct->consigne = (thePIDStruct->kp * nouvelleErreur)
+	    + (thePIDStruct->ki * thePIDStruct->sommeErreurs)
+	    + (thePIDStruct->kd
+		    * (nouvelleErreur - thePIDStruct->erreurPrecedente));
+
+    //mettre a jour l'erreur precedente pour la partie derivee
+    thePIDStruct->erreurPrecedente = nouvelleErreur;
+    }
+
