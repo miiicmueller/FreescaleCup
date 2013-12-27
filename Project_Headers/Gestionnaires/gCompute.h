9404dc725a0a0a27ada6a36cc471a2e82fa3ffc7
unknown <essayas.bekurezi@NE-EIN-BEKUREZE.ing.intra.eiaj.ch>
2013-12-27 22:50:45 +0100

    Implementation du differentiel

--- Project_Headers/Gestionnaires/gCompute.h
+++ Project_Headers/Gestionnaires/gCompute.h
@@ -25,7 +25,6 @@
 //-----------------------------------------------------------------------------
 // Déclaration des prototypes de fonctions
 //-----------------------------------------------------------------------------
-
 //------------------------------------------------------------------------
 // Initialisation de la structure de données de gInput
 //------------------------------------------------------------------------
@@ -35,5 +34,20 @@ void gCompute_Setup ( void );
 // aux autres modules via la structure des données de gInput
 //------------------------------------------------------------------------
 void gCompute_Execute ( void );
+
+//-----------------------------------------------------------------------------
+// Fonctions privees
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+// Compute differential
+// param : aAngleServo --> Consigne du servoMoteur
+//
+// Description : 	Avec aAngleServo a 0.51, pour 1 tour de la roue exterieur
+//					la roue interieur en fait 0.66.
+//
+//					Cette fonction calcul le differentiel à appliquer
+//					aux moteurs selon aAngleServo.
+//-----------------------------------------------------------------------------
+static void compute_differential(const float aAngleServo);
 
 #endif /* GCOMPUTE_H_ */
--- Sources/Gestionnaires/gCompute.c
+++ Sources/Gestionnaires/gCompute.c
@@ -18,15 +18,9 @@
 /* prototypes des fonctions statiques (propres au fichier) */
 static tPIDStruct thePIDServo;
 
-float aFreqMesTabMot1[FILTER_SIZE] =
-    {
-    0.0
-    };
+float aFreqMesTabMot1[FILTER_SIZE] = { 0.0 };
 
-float aFreqMesTabMot2[FILTER_SIZE] =
-    {
-    0.0
-    };
+float aFreqMesTabMot2[FILTER_SIZE] = { 0.0 };
 
 //-----------------------------------------------------------------------------
 //fonctions publiques
@@ -36,206 +30,196 @@ uint32_t median_filter_n(uint32_t *aTab, char aSize);
 //------------------------------------------------------------------------
 // Initialisation de la structure de données de gCompute
 //------------------------------------------------------------------------
-void gCompute_Setup(void)
-    {
-    gComputeInterStruct.gCommandeMoteurDroit = 0.0;
-    gComputeInterStruct.gCommandeMoteurGauche = 0.0;
-    gComputeInterStruct.gCommandeServoDirection = 0;
+void gCompute_Setup(void) {
+	gComputeInterStruct.gCommandeMoteurDroit = 0.0;
+	gComputeInterStruct.gCommandeMoteurGauche = 0.0;
+	gComputeInterStruct.gCommandeServoDirection = 0;
 
-    thePIDServo.kp = 0.006; //val max pour kp servo = 1/64
-    thePIDServo.ki = 0.00;
-    thePIDServo.kd = 0.006;
-    thePIDServo.consigne = 0; //ligne au milieu du champ de vision (étendue de -64 à 64)
-    thePIDServo.erreurPrecedente = 0;
-    thePIDServo.sommeErreurs = 0;
-    thePIDServo.coeffNormalisation = 1.0 / 64.0;
-    }
+	thePIDServo.kp = 0.006; //val max pour kp servo = 1/64
+	thePIDServo.ki = 0.00;
+	thePIDServo.kd = 0.006;
+	thePIDServo.consigne = 0; //ligne au milieu du champ de vision (étendue de -64 à 64)
+	thePIDServo.erreurPrecedente = 0;
+	thePIDServo.sommeErreurs = 0;
+	thePIDServo.coeffNormalisation = 1.0 / 64.0;
+}
 
 //------------------------------------------------------------------------
 // calcul des commandes à mettre pour les moteurs et le servo direction
 //
 //------------------------------------------------------------------------
-void gCompute_Execute(void)
-    {
-    //TODO : séparer le code dans différentes fonctions afin d'améliorer la lisibilité
+void gCompute_Execute(void) {
+	//TODO : séparer le code dans différentes fonctions afin d'améliorer la lisibilité
 
-    uint32_t valExposure = 0;
+	uint32_t valExposure = 0;
 
-    //lecture des donnees provenant du monitoring
-    if (gXbeeInterStruct.aPIDChangedServo)
-	{
-	thePIDServo.kp = gXbeeInterStruct.aGainPIDServo.gProprortionalGain;
-	thePIDServo.ki = gXbeeInterStruct.aGainPIDServo.gIntegraleGain;
-	thePIDServo.kd = gXbeeInterStruct.aGainPIDServo.gDerivativeGain;
+	//lecture des donnees provenant du monitoring
+	if (gXbeeInterStruct.aPIDChangedServo) {
+		thePIDServo.kp = gXbeeInterStruct.aGainPIDServo.gProprortionalGain;
+		thePIDServo.ki = gXbeeInterStruct.aGainPIDServo.gIntegraleGain;
+		thePIDServo.kd = gXbeeInterStruct.aGainPIDServo.gDerivativeGain;
 	}
 
-    //lecture des donnees provenant du monitoring
-    if (gXbeeInterStruct.aPIDChangedMotors)
-	{
-	//Motor1
-	mMotor1.aPIDData.kp =
-		gXbeeInterStruct.aGainPIDMotors.gProprortionalGain;
-	mMotor1.aPIDData.ki = gXbeeInterStruct.aGainPIDMotors.gIntegraleGain;
-	mMotor1.aPIDData.kd = gXbeeInterStruct.aGainPIDMotors.gDerivativeGain;
+	//lecture des donnees provenant du monitoring
+	if (gXbeeInterStruct.aPIDChangedMotors) {
+		//Motor1
+		mMotor1.aPIDData.kp =
+				gXbeeInterStruct.aGainPIDMotors.gProprortionalGain;
+		mMotor1.aPIDData.ki = gXbeeInterStruct.aGainPIDMotors.gIntegraleGain;
+		mMotor1.aPIDData.kd = gXbeeInterStruct.aGainPIDMotors.gDerivativeGain;
 
-	//Motor 2
-	mMotor2.aPIDData.kp =
-		gXbeeInterStruct.aGainPIDMotors.gProprortionalGain;
-	mMotor2.aPIDData.ki = gXbeeInterStruct.aGainPIDMotors.gIntegraleGain;
-	mMotor2.aPIDData.kd = gXbeeInterStruct.aGainPIDMotors.gDerivativeGain;
+		//Motor 2
+		mMotor2.aPIDData.kp =
+				gXbeeInterStruct.aGainPIDMotors.gProprortionalGain;
+		mMotor2.aPIDData.ki = gXbeeInterStruct.aGainPIDMotors.gIntegraleGain;
+		mMotor2.aPIDData.kd = gXbeeInterStruct.aGainPIDMotors.gDerivativeGain;
 	}
 
-    valExposure =
-	    (uint32_t) (((gXbeeInterStruct.aExpTime + 1.0) * 5000.0) + 1.0);
+	valExposure =
+			(uint32_t) (((gXbeeInterStruct.aExpTime + 1.0) * 5000.0) + 1.0);
 
-    //recherche de la ligne
-    static int16_t theLinePosition = 0;
-    bool isLineFound;
-    bool isStartStopFound;
-    int16_t LineAnalyze[128];
-    for (uint16_t i = 0; i < 128; i++)
-	{
-	LineAnalyze[i] = LineScanImage0[i];
+	//recherche de la ligne
+	static int16_t theLinePosition = 0;
+	bool isLineFound;
+	bool isStartStopFound;
+	int16_t LineAnalyze[128];
+	for (uint16_t i = 0; i < 128; i++) {
+		LineAnalyze[i] = LineScanImage0[i];
 	}
-    mTrackLine_FindLine(LineAnalyze, 128, &theLinePosition, &isLineFound,
-	    &isStartStopFound);
-    //---------------------------------------------------------------------------
-    //si la ligne est trouvee
-    if (isLineFound)
-	{
-	TFC_BAT_LED0_ON;
+	mTrackLine_FindLine(LineAnalyze, 128, &theLinePosition, &isLineFound,
+			&isStartStopFound);
+	//---------------------------------------------------------------------------
+	//si la ligne est trouvee
+	if (isLineFound) {
+		TFC_BAT_LED0_ON;
 
-	//filtrage de la position de la ligne
-	static uint8_t posFiltre = 0;
-	static int16_t theLinePositionTab[kTailleFiltre];
+		//filtrage de la position de la ligne
+		static uint8_t posFiltre = 0;
+		static int16_t theLinePositionTab[kTailleFiltre];
 
-	theLinePositionTab[posFiltre] = theLinePosition;
-	if (posFiltre < kTailleFiltre - 1)
-	    {
-	    posFiltre++;
-	    }
-	else
-	    {
-	    posFiltre = 0;
-	    }
-	theLinePosition = tMean(theLinePositionTab, kTailleFiltre);
+		theLinePositionTab[posFiltre] = theLinePosition;
+		if (posFiltre < kTailleFiltre - 1) {
+			posFiltre++;
+		} else {
+			posFiltre = 0;
+		}
+		theLinePosition = tMean(theLinePositionTab, kTailleFiltre);
+	} else {
+		TFC_BAT_LED0_OFF;
 	}
-    else
-	{
-	TFC_BAT_LED0_OFF;
-	}
-    // Mettre a jour le PID
-    tPID(&thePIDServo, (theLinePosition - 64));
+	// Mettre a jour le PID
+	tPID(&thePIDServo, (theLinePosition - 64));
 
-    //---------------------------------------------------------------------------
-    //TODO : faire une machine d'états pour contrôler la course(départ, arrivée, virage, ligne droite) afin de donner une bonne consigne aux moteurs
-    //si la ligne d'arrivee est trouvee
-    static bool isInRace = false;
-    static bool oldIsStartStopFound = false;
-    if (isStartStopFound && !(oldIsStartStopFound))
-	{
-	if (isInRace == true)
-	    {
-	    isInRace = false;
-	    }
-	else
-	    {
-	    isInRace = true;
-	    }
+	//---------------------------------------------------------------------------
+	//TODO : faire une machine d'états pour contrôler la course(départ, arrivée, virage, ligne droite) afin de donner une bonne consigne aux moteurs
+	//si la ligne d'arrivee est trouvee
+	static bool isInRace = false;
+	static bool oldIsStartStopFound = false;
+	if (isStartStopFound && !(oldIsStartStopFound)) {
+		if (isInRace == true) {
+			isInRace = false;
+		} else {
+			isInRace = true;
+		}
 	}
-    oldIsStartStopFound = isStartStopFound;
-    if (isInRace)
-	{
-	TFC_BAT_LED1_ON;
-	}
-    else
-	{
-	TFC_BAT_LED1_OFF;
+	oldIsStartStopFound = isStartStopFound;
+	if (isInRace) {
+		TFC_BAT_LED1_ON;
+	} else {
+		TFC_BAT_LED1_OFF;
 	}
 
-    //Filtrage des valeur du moteur
-    //Moteur 1 ;
-    if (mMotor1.aNumEchantillonsMot >= (FILTER_SIZE - 1))
-	{
-	uint32_t aCaptMedian = 0;
-	aCaptMedian = median_filter_n(mMotor1.aCaptTab, FILTER_SIZE);
-	gInputInterStruct.gFreq[0] = (F_COUNT) / aCaptMedian;
-	}
-    else
-	{
-	//On laisse la valeur d'entrée inchangée dans GInput
+	//Filtrage des valeur du moteur
+	//Moteur 1 ;
+	if (mMotor1.aNumEchantillonsMot >= (FILTER_SIZE - 1)) {
+		uint32_t aCaptMedian = 0;
+		aCaptMedian = median_filter_n(mMotor1.aCaptTab, FILTER_SIZE);
+		gInputInterStruct.gFreq[0] = (F_COUNT) / aCaptMedian;
+	} else {
+		//On laisse la valeur d'entrée inchangée dans GInput
 	}
 
-    //Moteur 2 ;
-    if (mMotor2.aNumEchantillonsMot >= (FILTER_SIZE - 1))
-	{
-	uint32_t aCaptMedian = 0;
-	aCaptMedian = median_filter_n(mMotor2.aCaptTab, FILTER_SIZE);
-	gInputInterStruct.gFreq[1] = (F_COUNT) / aCaptMedian;
-	}
-    else
-	{
-	//On laisse la valeur d'entrée inchangée dans GInput
+	//Moteur 2 ;
+	if (mMotor2.aNumEchantillonsMot >= (FILTER_SIZE - 1)) {
+		uint32_t aCaptMedian = 0;
+		aCaptMedian = median_filter_n(mMotor2.aCaptTab, FILTER_SIZE);
+		gInputInterStruct.gFreq[1] = (F_COUNT) / aCaptMedian;
+	} else {
+		//On laisse la valeur d'entrée inchangée dans GInput
 	}
 
-    //TODO : ajouter le contrôle des moteurs (différentiel, filtrage, PID)
+	//TODO : ajouter le contrôle des moteurs (différentiel, filtrage, PID)
+	
+	compute_differential(gComputeInterStruct.gCommandeServoDirection);
 
-    //---------------------------------------------------------------------------
-    //Appel des PID des moteurs
-    // PID Moteur 1
-    tPID(&mMotor1.aPIDData, (int16_t) (gInputInterStruct.gFreq[0] / 3.0)); // Frequence entre 0 et 100
-    // PID Moteur 2
-    tPID(&mMotor2.aPIDData, (int16_t) (gInputInterStruct.gFreq[1] / 3.0));
+	//---------------------------------------------------------------------------
+	//Appel des PID des moteurs
+	// PID Moteur 1
+	tPID(&mMotor1.aPIDData, (int16_t) (gInputInterStruct.gFreq[0] / 3.0)); // Frequence entre 0 et 100
+	// PID Moteur 2
+	tPID(&mMotor2.aPIDData, (int16_t) (gInputInterStruct.gFreq[1] / 3.0));
 
-    //---------------------------------------------------------------------------
-    //mise à jour des sorties de gCompute
-    gInputInterStruct.gPosCam1 = theLinePosition;
-    gComputeInterStruct.gCommandeServoDirection = thePIDServo.commande;
+	//---------------------------------------------------------------------------
+	//mise à jour des sorties de gCompute
+	gInputInterStruct.gPosCam1 = theLinePosition;
+	gComputeInterStruct.gCommandeServoDirection = thePIDServo.commande;
+	// TODO : Implementer la régulation des moteurs 
+	//   gComputeInterStruct.gCommandeMoteurGauche = mMotor1.aPIDData.commande;
+	//   gComputeInterStruct.gCommandeMoteurDroit = mMotor2.aPIDData.commande;
+	
+	gComputeInterStruct.gCommandeMoteurGauche = (gXbeeInterStruct.aMotorSpeedCons/100)* mMotor1.aDifferential;
+	gComputeInterStruct.gCommandeMoteurDroit = (gXbeeInterStruct.aMotorSpeedCons/100) * mMotor2.aDifferential;
 
-    gComputeInterStruct.gCommandeMoteurGauche = mMotor1.aPIDData.commande;
-    gComputeInterStruct.gCommandeMoteurDroit = mMotor2.aPIDData.commande;
-//    gComputeInterStruct.gCommandeMoteurGauche = 0.8;
-//    gComputeInterStruct.gCommandeMoteurDroit = 0.8;
-
-    }
+}
 
 //-----------------------------------------------------------------------------
 //fonctions statiques
 //-----------------------------------------------------------------------------
 
-uint32_t median_filter_n(uint32_t *aTab, char aSize)
-    {
-    uint32_t aCpyTab[aSize];
-    char i = 0, j = 0;
-    uint32_t aTemp;
+uint32_t median_filter_n(uint32_t *aTab, char aSize) {
+	uint32_t aCpyTab[aSize];
+	char i = 0, j = 0;
+	uint32_t aTemp;
 
-    //Copie du tableau
-    for (i = 0; i < aSize; i++)
-	{
-	aCpyTab[i] = aTab[i];
+	//Copie du tableau
+	for (i = 0; i < aSize; i++) {
+		aCpyTab[i] = aTab[i];
 	}
 
-    //On décale l'indice du tableau pour la prochaine mesure
-    for (i = (aSize - 1); i > 0; i--)
-	{
-	aTab[i] = aTab[i - 1];
+	//On décale l'indice du tableau pour la prochaine mesure
+	for (i = (aSize - 1); i > 0; i--) {
+		aTab[i] = aTab[i - 1];
 	}
 
-    //On trie le tableau
-    for (i = 0; i < aSize; i++)
-	{
-	for (j = 0; j < (aSize - 1); j++)
-	    {
-	    if (aCpyTab[j] > aCpyTab[j + 1])
-		{
-		// On swap les deux
-		aTemp = aCpyTab[j + 1];
-		aCpyTab[j + 1] = aCpyTab[j];
-		aCpyTab[j] = aTemp;
+	//On trie le tableau
+	for (i = 0; i < aSize; i++) {
+		for (j = 0; j < (aSize - 1); j++) {
+			if (aCpyTab[j] > aCpyTab[j + 1]) {
+				// On swap les deux
+				aTemp = aCpyTab[j + 1];
+				aCpyTab[j + 1] = aCpyTab[j];
+				aCpyTab[j] = aTemp;
+			}
 		}
-	    }
 	}
 
-    //On prend la valeur du milieu
-    return aCpyTab[(aSize - 1) / 2];
-    }
+	//On prend la valeur du milieu
+	return aCpyTab[(aSize - 1) / 2];
+}
+
+static void compute_differential(const float aAngleServo) {
+	
+	float m = -(0.34)/0.51;
+	
+	if((aAngleServo >=0.0))
+	{
+		mMotor1.aDifferential = 1.0;
+		mMotor2.aDifferential = m * aAngleServo + 1.0;
+		
+	}
+	else
+	{
+		mMotor1.aDifferential = -m * aAngleServo + 1.0;
+		mMotor2.aDifferential = 1.0;
+	}
+}
