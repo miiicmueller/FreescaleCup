9ca1536d40cbb0aa0c75d25415932deb89cfd3b4
CyrilleSavy <cyrille.savy@he-arc.ch>
2014-01-28 12:33:54 +0100

    param

---
+++ Project_Headers/parameters.h
@@ -0,0 +1,16 @@
+/*
+ * parameters.h
+ *
+ *  Created on: Jan 28, 2014
+ *      Author: cyrille.savy
+ */
+
+#ifndef PARAMETERS_H_
+#define PARAMETERS_H_
+
+#define kFULL_BRAKE -0.65
+#define kHALF_BRAKE -0.6
+#define kSPEED_DUTY 65.0
+#define K_SPEED_LOWEST 25.0
+
+#endif /* PARAMETERS_H_ */
